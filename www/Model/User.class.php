d117d46406be54d025a5b5037e6dce3c498eeb1b
ASSUNCAO Hugo <hassuncao@myges.fr>
2022-02-02 12:33:11 +0100

    Page accueil : Ajout des boutons "Connexion", "Inscription", "Contact"

--- www/Model/User.class.php
+++ www/Model/User.class.php
@@ -212,5 +212,34 @@ class User extends Sql
         ];
     }
 
+//Pour le formulaire de contact -Hugo
+    public function getContactForm(): array
+    {
+        return [
+            "config"=>[
+                "method"=>"POST",
+                "action"=>"",
+                "submit"=>"Contacter"
+            ],
+            'inputs'=>[
+                "email"=>[
+                    "type"=>"email",
+                    "placeholder"=>"Votre email ...",
+                    "required"=>true,
+                    "class"=>"inputForm",
+                    "id"=>"emailForm",
+                    "error"=>"Email incorrect"
+                ],
+                "message"=>[
+                    "type"=>"message",
+                    "placeholder"=>"Votre message ...",
+                    "required"=>true,
+                    "class"=>"inputForm",
+                    "id"=>"msgForm"
+                ]
+            ]
+        ];
+    }
+
 
 }
