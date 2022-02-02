a4f10ea84e3a62f267b2b78d6b12226872554fb6
ASSUNCAO Hugo <hassuncao@myges.fr>
2022-02-02 12:49:47 +0100

    Page accueil : Ajout des boutons "Connexion", "Inscription", "Contact"

--- www/Controller/Main.class.php
+++ www/Controller/Main.class.php
@@ -8,7 +8,16 @@ class Main {
 
     public function home()
     {
-        echo "Page d'accueil";
+        echo "<h1>Page d'accueil</h1>";
+        echo "
+            <nav class='menu'>
+                <ul>
+                    <li><a href='login'>Connexion</a></li>
+                    <li><a href='register'>Inscription</a></li>
+                    <li><a href='contact'>Contact</a></li>
+                </ul>
+            </nav>
+        ";
     }
 
 
