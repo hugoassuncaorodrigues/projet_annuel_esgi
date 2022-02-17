b4383d53181e07c4d98fbf08a87abdc4babc704a
muthuxv <muthulan.m@gmail.com>
2022-02-17 19:45:39 +0100

    page html front produits

---
+++ www/View/produits.view.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Page Front Produits</title>
+  <link rel="stylesheet" href="../dist/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="dropdown">
+                <button class="dropbtn">Catégorie
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+                </div>
+            </div>
+            <ul>
+      			<li><a href="#"><i class="fa fa-shopping-cart"></i> Panier</a></li>
+    		</ul>
+        </nav>
+    </header>
+	<div class="grid">
+		<div class="container">
+            <div class="row">
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg" alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+                <div>
+                    <img src="../dist/images/produits/daniel-ramos.jpg"  alt="Denim Jeans" style="max-width:100%">
+                    <h1>Jogging homme</h1>
+                    <p>20.99$</p>
+                    <p><button>Acheter</button></p>
+                </div>
+            </div>
+		</div>
+	</div>
+<!-- partial -->
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script><script  src="./script.js"></script>
+</body>
+</html>
