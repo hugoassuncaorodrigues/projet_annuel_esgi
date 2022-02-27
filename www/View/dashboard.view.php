1f69526ca566d42e0ac112483e435d00c1e0f475
CamilleBorra <cborra@hotmail.fr>
2022-02-27 18:08:38 +0100

    update media query

--- www/View/dashboard.view.php
+++ www/View/dashboard.view.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>CMS MyShopMaker</title>
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="./dist/main.css">
 
@@ -11,7 +12,7 @@
 <header>
 	</header>
 	<div class="nav-btn">Menu</div>
-	<div class="container">
+	<div class="container containerFlex">
 		<div class="sidebar">
 			<nav>
 				<a href="#"><span></span>
@@ -109,3 +110,4 @@
 
 </body>
 </html>
+ 
