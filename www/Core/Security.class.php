e756c48e9a4af38a4115823671088124987663d7
ASSUNCAO Hugo <hassuncao@myges.fr>
2022-03-03 17:56:38 +0100

    Verif connexion en cours

--- www/Core/Security.class.php
+++ www/Core/Security.class.php
@@ -7,8 +7,14 @@ class Security
 {
     public static function checkLogin($email, $password) {
         $user=new User();
-        $user->//voir la methode de camille GetOne;
-        password_verify($password,//mdp hash de la bdd);
+        $user->getOneBy(["email"=>"cborra@hotmail.fr"]);
+        $hash=????; //recuperation du mdp hasher dans la BDD
+        password_verify($password, $hash);
+        if (password_verify($password, $hash)) {
+            echo 'Le mot de passe est valide !';
+        } else {
+            echo 'Le mot de passe est invalide.';
+        }
     } 
 }
 ?>
